b5426721ac8690dd4d1ad633585204245589183d
tsuzan007 <tsuzanprajapati@gmail.com>
2018-01-31 16:22:15 -0500

    Added IntentService to handle the calculations. ProgressBar is shown when heavy traversal calculation is done. UI is free to acknowledge user's actions. EventBus is added to broadcast message from intentservice to the main  thread.

--- app/src/main/java/com/example/macbookpro/photon/ResultActivity.java
+++ app/src/main/java/com/example/macbookpro/photon/ResultActivity.java
@@ -1,25 +1,114 @@
 package com.example.macbookpro.photon;
 
+import android.app.IntentService;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.List;
 
 public class ResultActivity extends AppCompatActivity {
-    TextView textView_cost;
+    TextView textViewResult;
+    ProgressBar progressBar;
+    static int matrix[][];
+    private String result;
+    private SharedPreferences.Editor editor;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefs = getSharedPreferences("MySharedPreference", MODE_PRIVATE);
         setContentView(R.layout.activity_result);
-        textView_cost = (TextView) findViewById(R.id.cost);
-        seeTraverse();
+        textViewResult = findViewById(R.id.textView_Result);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
     }
 
-    public void seeTraverse() {
-        MatrixTraverse mat = new MatrixTraverse(TableActivity.arr);
-        List<String> trList = mat.traverse();
-        textView_cost.setText(" " + trList.get(2) + "\n Traversecost:  " + trList.get(0) + " \n Traverse path " + trList.get(1));
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (result == null) {
+            Intent i = getIntent();
+            matrix = (int[][]) i.getSerializableExtra("matrix");
+            Intent serviceIntent = new Intent(this, TraverseService.class);
+            startService(serviceIntent);
+        } else {
+            progressBar.setVisibility(View.INVISIBLE);
+            textViewResult.setText(result);
+        }
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    /**
+     * Receives a message via EventBus and display the result.
+     *
+     * @param message message to acknowledge.
+     */
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(String message) {
+        progressBar.setVisibility(View.INVISIBLE);
+        result = message;
+        textViewResult.setText(message);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("result", result);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        result = savedInstanceState.getString("result");
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
+
+class TraverseService extends IntentService {
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     */
+    public TraverseService() {
+        super("This is intent service");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        MatrixTraverse mat = new MatrixTraverse(ResultActivity.matrix);
+        final List<String> trList = mat.traverse();
+        Log.e("mes", "in the intent service");
+        EventBus.getDefault().post(" " + trList.get(2) + "\n Traversecost:  " + trList.get(0) + " \n Traverse path " + trList.get(1));
+
+
+    }
+}
+

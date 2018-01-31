45205528636f51f651b77ad9e5e59547c30f1d21
tsuzan007 <tsuzanprajapati@gmail.com>
2018-01-31 15:58:45 -0500

    Comments added and file formatted.

--- app/src/main/java/com/example/macbookpro/photon/MatrixTraverse.java
+++ app/src/main/java/com/example/macbookpro/photon/MatrixTraverse.java
@@ -4,99 +4,122 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
-public class MatrixTraverse extends Matrix implements ElementValidation  {
-    int max_width, max_height, resultCost = Integer.MAX_VALUE;
-     int MAX_COST;
-    String msg = "";
-//    String resultPath = "";
-    boolean conditonBreak = false;
-    Path p;
-    static int finalCost=0;
+public class MatrixTraverse extends Matrix implements ElementValidation {
+    private int max_width, max_height, resultCost = Integer.MAX_VALUE;
+    private int maxcost = 50;
+    private int MAX_COST;
+    private boolean conditonBreak = false;
+    private Path path;
+    static int finalCost = 0;
     static String finalPath;
     static String isPath;
 
-
-    public MatrixTraverse(int[][] a) {
-        super(a);
-        p=new Path("");
-        max_width=getMax_width();
-        max_height=getMax_height();
-        int maxcost=50;
-        if(max_width > 10)
-        {
-            maxcost=21;
+    /**
+     * Constructor of the class. Initialize matrix and other parameters.
+     *
+     * @param mgrid matrix to traverse
+     */
+    public MatrixTraverse(int[][] mgrid) {
+        super(mgrid);
+        path = new Path("");
+        max_width = getMax_width();
+        max_height = getMax_height();
+        if (max_width > 10) {
+            maxcost = 21;
         }
-        MAX_COST= maxcost;
-        if((max_height==0)&&(max_width==0))
-        {
+        MAX_COST = maxcost;
+        if ((max_height == 0) && (max_width == 0)) {
             List<String> trv = new ArrayList<>();
-            trv.add(""+-1);
+            trv.add("" + -1);
         }
 
     }
-    public void start() {
+
+
+    /*
+        Traverses in the matrix and finds the path of lowest cost.
+    */
+    public void startTraverse() {
         int j = 0;
         for (int i = 0; i < max_height; i++) {
             System.out.println("row " + (i + 1) + " col " + (j + 1));
-            f(i, j, 0, "");
+            findPath(i, j, 0, "");
 
         }
-      display();
-  }
+        display();
+    }
+
+
+    /**
+     * Prints the final result in the console
+     */
     private void display() {
         System.out.println("Final Out!!");
         if (conditonBreak) {
             System.out.println("No");
-            isPath="No";
-        }
-        else {
+            isPath = "No";
+        } else {
             System.out.println("Yes");
-            isPath="Yes";
+            isPath = "Yes";
         }
-        if(Integer.MAX_VALUE==resultCost)resultCost=0;
+        if (Integer.MAX_VALUE == resultCost) resultCost = 0;
         System.out.println(resultCost);
-       finalCost = resultCost;
-        System.out.println("["+p.getPath().replaceAll(",", " ").trim()+"]");
-       finalPath="["+p.getPath().replaceAll(",", " ").trim()+"]";
+        finalCost = resultCost;
+        System.out.println("[" + path.getPath().replaceAll(",", " ").trim() + "]");
+        finalPath = "[" + path.getPath().replaceAll(",", " ").trim() + "]";
 
 
     }
 
 
+    /**
+     * Main method to find the path of the lowest cost.
+     * Traverse along the matrix and find the path
+     *
+     * @return Lists of integer that represent rows traversed to find the path
+     */
+    public List<String> traverse() {
+        this.startTraverse();
+        String validChk = matrixchk(getMgrid());
+        if (validChk.equalsIgnoreCase("valid")) {
+            System.out.println("TRAVERSE " + finalCost);
+            List<String> traverseList = new ArrayList<>();
+            String s = Integer.toString(finalCost);
+            String cond;
+            if (conditonBreak)
+                cond = "No";
+            else {
+                cond = "Yes";
+            }
 
-    public List<String> traverse(){
-       this.start();
-        String validChk=matrixchk(getMgrid());
-     if(validChk.equalsIgnoreCase("valid")) {
-         System.out.println("TRAVERSE " + finalCost);
-         List<String> traverseList = new ArrayList<>();
-         String s = Integer.toString(finalCost);
-         String cond;
-         if (conditonBreak)
-             cond = "No";
-         else {
-             cond = "Yes";
-         }
-
-         traverseList.add(s);
-         traverseList.add(finalPath);
-         traverseList.add(isPath);
-         return traverseList;
-     }
-        else{
-         List<String> traverseList = new ArrayList<>();
-         traverseList.add("invalid Matrix");
-         String val = Integer.toString(-1);
-         traverseList.add(val);
-         return traverseList;
+            traverseList.add(s);
+            traverseList.add(finalPath);
+            traverseList.add(isPath);
+            return traverseList;
+        } else {
+            List<String> traverseList = new ArrayList<>();
+            traverseList.add("invalid Matrix");
+            String val = Integer.toString(-1);
+            traverseList.add(val);
+            return traverseList;
 
 
-     }
+        }
 
     }
 
-    public void f(int row, int col, int currentCost, String currentPath) {
+
+    /**
+     * Finds the path of lowest cost.
+     * Invoked by traverse method.
+     * <p>
+     *
+     * @param row         current row.
+     * @param col         current col.
+     * @param currentCost cost till the current position.
+     * @param currentPath path till the current position.
+     */
+    public void findPath(int row, int col, int currentCost, String currentPath) {
 
         List<Position> nextPosiList = null;
         boolean conditionMaxCost = false;
@@ -111,48 +134,45 @@ public class MatrixTraverse extends Matrix implements ElementValidation  {
         }
         if (nextPosiList != null) {// sort the position
             for (Position p : nextPosiList) {
-                f(p.getX(), p.getY(), currentCost, currentPath);
+                findPath(p.getX(), p.getY(), currentCost, currentPath);
                 System.out.println(p + " " + currentPath);
             }
         } else {
-// current set Finished
+            // current set Finished
             if (resultCost >= currentCost) {
                 if (!conditionMaxCost) {
                     resultCost = currentCost;
                     //resultPath = currentPath;
-                    p.setPath(currentPath);
+                    path.setPath(currentPath);
                     conditonBreak = false;
-                    System.out.println("success currentPath " + p.getPath() + " cost " + resultCost);
-                } else if (p.getPath().length() < currentPath.length()) {
+                    System.out.println("success currentPath " + path.getPath() + " cost " + resultCost);
+                } else if (path.getPath().length() < currentPath.length()) {
                     resultCost = currentCost;
-                   // resultPath = currentPath;
-                    p.setPath(currentPath);
+                    // resultPath = currentPath;
+                    path.setPath(currentPath);
                     conditonBreak = true;
-                    System.out.println("break currentPath " + p.getPath() + " cost " + resultCost);
+                    System.out.println("break currentPath " + path.getPath() + " cost " + resultCost);
                 }
 
-            }
-            else if(currentPath.length() >= max_width*2 && conditonBreak){
+            } else if (currentPath.length() >= max_width * 2 && conditonBreak) {
                 if (!conditionMaxCost) {
                     resultCost = currentCost;
-                   // resultPath = currentPath;
-                    p.setPath(currentPath);
+                    // resultPath = currentPath;
+                    path.setPath(currentPath);
                     conditonBreak = false;
-                    System.out.println("full success currentPath " + p.getPath() + " cost " + resultCost);
-                }
-                else {
+                    System.out.println("full success currentPath " + path.getPath() + " cost " + resultCost);
+                } else {
                     resultCost = currentCost;
-                   // resultPath = currentPath;
-                    p.setPath(currentPath);
+                    // resultPath = currentPath;
+                    path.setPath(currentPath);
                     conditonBreak = true;
-                    System.out.println("full break currentPath " + p.getPath() + " cost " + resultCost);
+                    System.out.println("full break currentPath " + path.getPath() + " cost " + resultCost);
 
                 }
-            }
-            else if(p.getPath().length() < currentPath.length() && (conditionMaxCost)){
+            } else if (path.getPath().length() < currentPath.length() && (conditionMaxCost)) {
                 resultCost = currentCost;
                 //resultPath = currentPath;
-                p.setPath(currentPath);
+                path.setPath(currentPath);
                 conditonBreak = true;
             }
 
@@ -160,59 +180,72 @@ public class MatrixTraverse extends Matrix implements ElementValidation  {
 
     }
 
+
+    /**
+     * Returns the adjacent position of the current position.
+     * Adjacent postions are choices we can travel along the matrix.
+     * Invoked by your subclass's constructor.
+     * <p>
+     *
+     * @param row current row.
+     * @param col current col.
+     */
     public List<Position> getAdjPosition(int row, int col) {
-            List<Position> list = new ArrayList<>();
-            if(row<0 || col<0){
-                return list;
-            }
-            int nexCol = col + 1;
-
-            if (getMgrid()[row].length > nexCol) {// same row next column
-                Position p = new Position(row, nexCol);
-                list.add(p);
-            } else {
-                return null; // now nextCol then end of array.
-            }
-
-            if (max_height > row + 1) {
-                Position p = new Position(row + 1, nexCol);
-                list.add(p);
-            } else {
-                Position p = new Position(0, nexCol);
-                list.add(p);
-            }
-
-            if (row - 1 >= 0) {
-                Position p = new Position(row - 1, nexCol);
-                list.add(p);
-            } else {
-                Position p = new Position(max_height - 1, nexCol);
-                list.add(p);
-            }
-
+        List<Position> list = new ArrayList<>();
+        if (row < 0 || col < 0) {
             return list;
+        }
+        int nexCol = col + 1;
+
+        if (getMgrid()[row].length > nexCol) {// same row next column
+            Position p = new Position(row, nexCol);
+            list.add(p);
+        } else {
+            return null; // now nextCol then end of array.
+        }
+
+        if (max_height > row + 1) {
+            Position p = new Position(row + 1, nexCol);
+            list.add(p);
+        } else {
+            Position p = new Position(0, nexCol);
+            list.add(p);
+        }
+
+        if (row - 1 >= 0) {
+            Position p = new Position(row - 1, nexCol);
+            list.add(p);
+        } else {
+            Position p = new Position(max_height - 1, nexCol);
+            list.add(p);
+        }
+
+        return list;
 
     }
 
-    public String matrixchk(int[][] matrixTest)
-    {
+    /**
+     * Checks if the matrix is valid or not
+     *
+     * @param matrixTest matrix to check
+     * @return the result valid/not
+     */
+    public String matrixchk(int[][] matrixTest) {
         boolean nonNumeric;
-        String val="";
-        if(matrixTest.length < 0){
+        String val = "";
+        if (matrixTest.length < 0) {
             return "invalid";
-        }
-        else{
-            int row=matrixTest.length;
-            int col=matrixTest[0].length;
-            for(int i=0;i<row;i++){
-                for(int j=0;j<col;j++){
-                    nonNumeric=chkNonNumeric(""+matrixTest[i][j]);
-                    if(nonNumeric==false){
-                        val="invalid";
+        } else {
+            int row = matrixTest.length;
+            int col = matrixTest[0].length;
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    nonNumeric = chkNonNumeric("" + matrixTest[i][j]);
+                    if (nonNumeric == false) {
+                        val = "invalid";
                         break;
-                    }
-                    else{
-                        val="valid";
+                    } else {
+                        val = "valid";
                     }
                 }
             }
@@ -224,12 +257,10 @@ public class MatrixTraverse extends Matrix implements ElementValidation  {
     @Override
     public boolean chkNonNumeric(String num) {
 
-        boolean isNumber=true;
-        try{
+        boolean isNumber = true;
+        try {
             Integer.parseInt(num);
-        }
-        catch (NumberFormatException ex)
-        {
+        } catch (NumberFormatException ex) {
             isNumber = false;
         }
         return isNumber;

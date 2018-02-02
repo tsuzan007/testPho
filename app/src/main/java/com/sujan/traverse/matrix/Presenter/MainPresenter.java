bc45f4b5148ed5267f874715a81ebadafd0eef05
tsuzan007 <tsuzanprajapati@gmail.com>
2018-02-02 15:09:28 -0500

    Presenter class which stores user inputs into array. Added method to initialize the matrices.

---
+++ app/src/main/java/com/sujan/traverse/matrix/Presenter/MainPresenter.java
@@ -0,0 +1,54 @@
+package com.sujan.traverse.matrix.Presenter;
+
+
+import com.sujan.traverse.matrix.HelperClass.Position;
+
+/**
+ * Created by macbookpro on 2/2/18.
+ */
+
+public class MainPresenter implements PresenterOps {
+    public static String matrix[][];
+    public static int int_matrix[][];
+    public static int row = 0;
+    public static int col = 0;
+
+    @Override
+    public boolean onClick_submit() {
+        boolean error = false;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                try {
+                    int_matrix[i][j] = Integer.parseInt(matrix[i][j]);
+                } catch (NumberFormatException nfe) {
+                    nfe.printStackTrace();
+                    error = true;
+                    break;
+                }
+            }
+            if (error == true)
+                break;
+        }
+        return error;
+
+    }
+
+    @Override
+    public void onTableTextChange(Position p, CharSequence c) {
+        MainPresenter.matrix[p.getX()][p.getY()] = c.toString();
+    }
+
+    @Override
+    public void initMatrices() {
+        for (int i = 0; i < MainPresenter.row; i++) {
+            for (int j = 0; j < MainPresenter.col; j++) {
+                MainPresenter.matrix[i][j] = "";
+                MainPresenter.int_matrix[i][j] = 0;
+
+
+            }
+        }
+    }
+
+
+}

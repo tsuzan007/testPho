843742f2c6da3bfc4d0ce8e16da07f13d250a6fc
tsuzan007 <tsuzanprajapati@gmail.com>
2018-02-01 21:54:52 -0500

    Unit test class for Path class.

---
+++ app/src/test/java/com/sujan/traverse/matrix/PathTest.java
@@ -0,0 +1,51 @@
+package com.sujan.traverse.matrix;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by macbookpro on 2/1/18.
+ */
+
+public class PathTest {
+
+    @Test
+    public void PathConstruct_NotNull() {
+        Path p = new Path("1 2 3");
+        assertNotNull(p);
+
+    }
+
+    @Test
+    public void setPath_IsSuccess() {
+        Path p = new Path("1 2 3");
+        p.setPath("4 5 6");
+        assertEquals("4 5 6", p.getPath());
+    }
+
+
+    @Test
+    public void getPath_ReturnsPath() {
+        Path p = new Path("1 2 3");
+        assertEquals("1 2 3", p.getPath());
+    }
+
+    @Test
+    public void setCost_IsSuccess() {
+        Path p = new Path("1 2 3");
+        p.setCost(1);
+        assertEquals(1, p.getCost());
+    }
+
+
+    @Test
+    public void getCost_ReturnsCost() {
+        Path p = new Path("1 2 3");
+        assertEquals(0, p.getCost());
+    }
+
+
+
+}

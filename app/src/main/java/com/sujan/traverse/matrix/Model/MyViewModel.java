941d386eb5d4c7b54d7ccfde2c784c76b156fd7e
tsuzan007 <tsuzanprajapati@gmail.com>
2018-02-02 15:06:29 -0500

    A ViewModel class that handles configuration changes. It saves instance of presenter while configuration changes.

---
+++ app/src/main/java/com/sujan/traverse/matrix/Model/MyViewModel.java
@@ -0,0 +1,27 @@
+package com.sujan.traverse.matrix.Model;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.sujan.traverse.matrix.Presenter.MainPresenter;
+
+/**
+ * Created by macbookpro on 2/2/18.
+ */
+
+/**
+ * This class saves the instance of mainPresenter during configuration changes.
+ */
+public class MyViewModel extends ViewModel {
+    private MainPresenter mainPresenter;
+
+
+    public MainPresenter getMainPresenter(){
+        if(mainPresenter==null){
+            mainPresenter=new MainPresenter();
+
+        }
+        return mainPresenter;
+    }
+}

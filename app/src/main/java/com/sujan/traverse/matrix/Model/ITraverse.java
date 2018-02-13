c6118c0e3e32b83694aa6c21c6f299e2937f8870
tsuzan007 <tsuzanprajapati@gmail.com>
2018-02-13 12:45:23 -0500

    provides interface to traverse in the matrix and find the path. getAdjPosition implementation will provide possible path from a position in matrix

---
+++ app/src/main/java/com/sujan/traverse/matrix/Model/ITraverse.java
@@ -0,0 +1,15 @@
+package com.sujan.traverse.matrix.Model;
+
+import com.sujan.traverse.matrix.Model.HelperClass.Position;
+
+import java.util.List;
+
+/**
+ * Created by macbookpro on 2/12/18.
+ */
+
+public interface ITraverse {
+    List<String> traverse(int[][] matrix);
+    List<Position> getAdjPosition(int[][] matrix,int row, int col);
+
+}
